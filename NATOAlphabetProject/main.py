de335494ac6de2bd85080f9bc32f5bb8bb287301
Carmen Creswell <carmencreswell@gmail.com>
2022-02-08 10:48:46 -0600

    Finish NATO phonetic alphabet project

--- NATOAlphabetProject/main.py
+++ NATOAlphabetProject/main.py
@@ -1,9 +1,11 @@
-from pandas import DataFrame, read_csv
+from pandas import read_csv
 
 df = read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter:row.code for (index,row) in df.iterrows()}
 
-print(alphabet_dict)
+word = ""
+while word != "EXIT":
+    word = input("Enter a word to get the phonetic alphabet version of: ").upper()
+    phonetic_word = [alphabet_dict.get(letter) for letter in word]
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
+    print(f"{' '.join(phonetic_word)}\n")
