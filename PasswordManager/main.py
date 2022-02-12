e29223628f8ba564c1b26a28a9fe64cc74fe8417
Carmen Creswell <carmencreswell@gmail.com>
2022-02-12 17:53:17 -0600

    Add confirmation dialog for adding data to PasswordManager

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -1,15 +1,28 @@
-from tkinter import Button, Canvas, Entry, Label, Tk, PhotoImage, END
+from tkinter import Button, Canvas, Entry, Label, Tk, PhotoImage, END, messagebox
+import random
 
 # TODO: PASSWORD GENERATOR 
+def generate_pw():
+    password = ""
+
+    password_input.delete(0, END)
+    password_input.insert(0, password)
 
 # Absolutely NOT a secure way to store passwords, this is just practice for now!! I would never store a user's password in plain text and this is only meant to be run on a local machine
 def write_data():
-    with open("data.txt", "a") as file:
-        file.write(f"{website_input.get()} | {email_username_input.get()} | {password_input.get()}\n")
+    website = website_input.get()
+    username = email_username_input.get()
+    password = password_input.get()
 
-    website_input.delete(0,END)
-    email_username_input.delete(0, END)
-    password_input.delete(0, END) 
+    is_correct = messagebox.askokcancel(title=website, message=f"Are these details correct?\nWebsite: {website}\nUsername: {username}\nPassword: {password}" )
+
+    if is_correct: 
+        with open("data.txt", "a") as file:
+            file.write(f"{website} | {username} | {password}\n")
+
+        website_input.delete(0,END)
+        email_username_input.delete(0, END)
+        password_input.delete(0, END) 
 
 win = Tk()
 win.title("Password Manager")
@@ -44,7 +57,7 @@ password_input = Entry(width=21)
 password_input.grid(column=1, row=3)
 
 # Generate Password button
-generate_pw_button = Button(text="Generate Password")
+generate_pw_button = Button(text="Generate Password", command=generate_pw)
 generate_pw_button.grid(column=2, row=3)
 
 # Add button
