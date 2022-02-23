65d00020f23d64f3609baa397e6d813228bf76a6
Carmen Creswell <carmencreswell@gmail.com>
2022-02-23 14:39:21 -0600

    Store passwords in json instead of text file and add search ability to PasswordManager

--- PasswordManager/main.py
+++ PasswordManager/main.py
@@ -2,6 +2,8 @@ from tkinter import Button, Canvas, Entry, Label, Tk, PhotoImage, END, messagebo
 from random import choice, randint, shuffle
 from string import ascii_letters, punctuation
 
+import json
+
 def generate_pw():
     letters = [choice(ascii_letters) for _ in range(8)]
     punct = [choice(punctuation) for _ in range(2)]
@@ -21,6 +23,12 @@ def write_data():
     website = website_input.get().strip()
     username = email_username_input.get().strip()
     password = password_input.get().strip()
+    new_data = { 
+        website: {
+            "username": username, 
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showwarning(title="Empty fields", message="Please don't leave any fields empty!")
@@ -29,12 +37,33 @@ def write_data():
     is_correct = messagebox.askokcancel(title=website, message=f"Are these details correct?\nWebsite: {website}\nUsername: {username}\nPassword: {password}" )
 
     if is_correct: 
-        with open("data.txt", "a") as file:
-            file.write(f"{website} | {username} | {password}\n")
-
+        try:
+            with open("data.json", "r") as file:
+                data = json.load(file)       
+        except FileNotFoundError:
+            with open("data.json", "w") as file:
+                json.dump(new_data, file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "w") as file:
+                json.dump(data, file, indent=4)
+            
         website_input.delete(0,END)
         email_username_input.delete(0, END)
         password_input.delete(0, END) 
+
+def search_credentials(website):
+    try:
+        with open("data.json", "r") as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        messagebox.showerror(message="No credentials stored yet")
+    else: 
+        try:
+            credentials = data[website]
+            messagebox.showinfo(message=f"{website.title()}\nUsername: {credentials['username']}\nPassword: {credentials['password']}")
+        except KeyError:
+            messagebox.showerror(message=f"Credentials for website {website} not found")
 
 win = Tk()
 win.title("Password Manager")
@@ -50,9 +79,13 @@ canvas.grid(column=1, row=0)
 website_label = Label(text="Website:")
 website_label.grid(column=0, row=1)
 
-website_input = Entry(width=38)
-website_input.grid(column=1, row=1, columnspan=2)
+website_input = Entry(width=21)
+website_input.grid(column=1, row=1)
 website_input.focus()
+
+# Website Search button
+search_button = Button(text="Search", width=13, command= lambda: search_credentials(website_input.get()))
+search_button.grid(column=2, row=1)
 
 # Email/Username input
 email_username_label = Label(text="Email/Username:")
