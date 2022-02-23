57f9e88214626ff5018ad459167fadb97b9f0a4f
Carmen Creswell <carmencreswell@gmail.com>
2022-02-22 21:49:12 -0600

    Add main to NATO alphabet

--- NATOAlphabetProject/main.py
+++ NATOAlphabetProject/main.py
@@ -1,14 +1,18 @@
 from pandas import read_csv
 
-df = read_csv("nato_phonetic_alphabet.csv")
-alphabet_dict = {row.letter:row.code for (index,row) in df.iterrows()}
+def main():
+    df = read_csv("nato_phonetic_alphabet.csv")
+    alphabet_dict = {row.letter:row.code for (index,row) in df.iterrows()}
 
-word = ""
-while word != "EXIT":
-    word = input("Enter a word to get the phonetic alphabet version of: ").upper()
-    phonetic_word = [alphabet_dict.get(letter) for letter in word]
+    word = ""
+    while word != "EXIT":
+        word = input("Enter a word to get the phonetic alphabet version of: ").upper()
+        phonetic_word = [alphabet_dict.get(letter) for letter in word]
 
-    try: 
-        print(f"{' '.join(phonetic_word)}\n")
-    except TypeError:
-        print("Please only enter letters\n")
+        try: 
+            print(f"{' '.join(phonetic_word)}\n")
+        except TypeError:
+            print("Please only enter letters\n")
+
+if __name__ == "__main__":
+    main()
